65f4a757428d55d7e37a7f8e69e93fe45fa9c440
Andy Jones <andyjones.ed@gmail.com>
2013-10-27 02:49:26 +0000

    Forgot to commit atmospheric element files earlier

--- Assets/C#/Source/Elements/AtmosphericElement.cs
+++ Assets/C#/Source/Elements/AtmosphericElement.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using UnityEngine;
+
+public class AtmosphericElement : IGenerableAtmosphericElement
+{
+    public int Index { get; set; }
+
+    public int VertexIndex { get; set; }
+
+    public float Radius { get; set; }
+
+    public float Height { get; set; }
+
+    public Vector3 Direction { get; set; }
+
+    public Boundary[] Boundaries { get; set; }
+}
---
+++ Assets/C#/Source/Elements/IGenerableAtmosphericElement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public interface IGenerableAtmosphericElement
+{
+    int Index { set; }
+
+    int VertexIndex { set; }
+
+    float Radius { set; }
+
+    float Height { set; }
+
+    Vector3 Direction { set; }
+
+    Boundary[] Boundaries { set; }
+}
